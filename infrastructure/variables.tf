30018d5005b2f2b4f866068c27e079988623af25
Filip <78978614+HermanPlay@users.noreply.github.com>
2025-04-19 16:25:15 +0200

    Feat/deploy workflow (#5)

---
+++ infrastructure/variables.tf
@@ -0,0 +1,29 @@
+variable "project_id" {
+  description = "A GCP project ID."
+  type        = string
+}
+
+variable "region" {
+  description = "A default GCP region"
+  type        = string
+}
+
+variable "zone" {
+  description = "A default GCP zone"
+  type        = string
+}
+
+variable "name" {
+  description = "Name of kubernetes cluster."
+  type        = string
+}
+
+variable "node_count" {
+  description = "A number of kubernetes nodes."
+  type        = number
+}
+
+variable "node_size" {
+  description = "A size of a kubernetes node."
+  type        = string
+}
